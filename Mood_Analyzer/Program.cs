5962895378795cc99cc797bf272d759d807407a2
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-19 22:40:19 +0530

    Added:Mood analayzer program

---
+++ Mood_Analyzer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mood_Analyzer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyzer Problem");
+
+
+        }
+    }
+}
